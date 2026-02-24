136d0dd617af0d57cdebb9428e9a30f463541995
joao-zip <oliveira.joao@unifesp.br>
2026-02-24 00:27:05 -0300

    fix: performance things

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://joao-zip.github.io',
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  compressHTML: true,
 });
